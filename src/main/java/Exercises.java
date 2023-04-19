7e347b81637c75c0277330b3416feb6dc566fd02
courtneyjohnson238 <courtneyjohnson238@gmail.com>
2023-04-19 14:03:50 -0400

    added files

---
+++ src/main/java/Exercises.java
@@ -0,0 +1,20 @@
+public class Exercises {
+    public static void main(String[] args) {
+
+            int i = 1;
+
+
+            while (i<=5){
+                System.out.println("I love Java");
+                i++;
+
+            }
+
+    }   
+
+    }
+
+
+
+
+
